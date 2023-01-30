e029866e6ca84f084c1869728c247f6fb48a8f90
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-01-30 11:31:39 +0100

    Basic Explaination on Event Loops

--- 2_How_Node_Works/event_loop.js
+++ 2_How_Node_Works/event_loop.js
@@ -1,9 +1,32 @@
-const fs = require("fs");
+const fs = require('fs');
+const crypto = require('crypto');
+const start = Date.now();
 
-setTimeout(() => console.log("Timer 1 finished"), 0);
-setImmediate(() => console.log("Immediate 1 Finished"))
+// process.env.UV_THREADPOOL_SIZE = 4;
 
-fs.readFile('test_file.txt', () => {
-    console.log(`I/O finished`)
-})
+setTimeout(() => console.log('Timer 1 finished'), 0);
+setImmediate(() => console.log('Immediate 1 Finished'));
 
+fs.readFile(`${__dirname}/test_file.txt`, () => {
+  console.log(`I/O finished`);
+  console.log(`------------`);
+  setTimeout(() => console.log('Timer 2 finished'), 0);
+  setTimeout(() => console.log('Timer 3 finished'), 3000);
+  setImmediate(() => console.log('Immediate 2 Finished'));
+  process.nextTick(() => console.log(`Process.nectTick `));
+
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password Encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password Encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password Encrypted');
+  });
+  crypto.pbkdf2('password', 'salt', 100000, 1024, 'sha512', () => {
+    console.log(Date.now() - start, 'Password Encrypted');
+  });
+});
+
+console.log(`Hello From Top Level Code`);
