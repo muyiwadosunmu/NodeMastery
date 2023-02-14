f8784c9e1aaf9d712b31ac7c29c4b26cbf923526
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-02-14 14:23:59 +0100

    Worked on Advanced Filtering

--- 4_Natours_API/Controllers/tourControllers.js
+++ 4_Natours_API/Controllers/tourControllers.js
@@ -2,16 +2,31 @@ const Tour = require('../models/tourModels');
 
 const getAllTours = async (req, res) => {
   try {
+    console.log(req.query);
+    // BUILD THE QUERY
     const queryObject = { ...req.query }; //We're making a hardCopy
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObject[el]);
 
-    const tours = await Tour.find(queryObject);
-    // const tours = await Tour.find()
+    //2) Advanced Filtering
+    let queryStr = JSON.stringify(queryObject);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
+
+    const query = Tour.find(JSON.parse(queryStr));
+
+    //{difficulty: 'easy', duration:{$gte:5}}
+
+    // const query = Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
+
+    // EXEXCUTE QUERY
+    const tours = await query;
+
+    // SEND RESPONSE
     return res.status(200).json({
       status: 'Success',
       requestedAt: req.requestTime,
