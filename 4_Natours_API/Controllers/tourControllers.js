9b652dfd38c7a3af0f0a5c60d406cdd23ebc8e3f
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-02-16 10:23:21 +0100

    Learnt Advanced filtering and Sorting

--- 4_Natours_API/Controllers/tourControllers.js
+++ 4_Natours_API/Controllers/tourControllers.js
@@ -1,29 +1,46 @@
 const Tour = require('../models/tourModels');
 
+// Method 1
+// const tours = await Tours.find({
+//   duration: 5,
+//   difficulty: 'easy',
+// });
+//{difficulty: 'easy', duration:{$gte:5}}
+
+// Method 2
+// const query = Tour.find()
+//   .where('duration')
+//   .equals(5)
+//   .where('difficulty')
+//   .equals('easy');
+
+// EXEXCUTE QUERY
+
 const getAllTours = async (req, res) => {
   try {
     console.log(req.query);
     // BUILD THE QUERY
+    // 1A) FILTERING
     const queryObject = { ...req.query }; //We're making a hardCopy
     const excludedFields = ['page', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => delete queryObject[el]);
 
-    //2) Advanced Filtering
+    //1B) Advanced Filtering
     let queryStr = JSON.stringify(queryObject);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    console.log(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
 
-    const query = Tour.find(JSON.parse(queryStr));
+    //2 SORTING
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
 
-    //{difficulty: 'easy', duration:{$gte:5}}
+      query = query.sort(sortBy);
+      // sort('price ratingsAverage'), if there's a tie
+    } else {
+      query = query.sort('-createdAt');
+    }
 
-    // const query = Tour.find()
-    //   .where('duration')
-    //   .equals(5)
-    //   .where('difficulty')
-    //   .equals('easy');
-
-    // EXEXCUTE QUERY
+    // EXECUTE QUERY
     const tours = await query;
 
     // SEND RESPONSE
