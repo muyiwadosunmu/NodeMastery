05ca8b787c5b2e968cba6a227e17c80eddbeed1d
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-02-28 11:37:24 +0100

    Separated Production Errors and Development Errors

--- 4_Natours_API/Controllers/errorController.js
+++ 4_Natours_API/Controllers/errorController.js
@@ -1,9 +1,42 @@
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    err: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational, trusted error : Send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      // err: err,
+      message: err.message,
+      // stack: err.stack,
+    });
+
+    // Programming Error or other unknown error:
+  } else {
+    // 1. Log the Error
+    console.error(`Error`, err);
+    //2. Send Generic Message
+    res.status(500).json({
+      status: 'Error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   console.log(err.stack);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
