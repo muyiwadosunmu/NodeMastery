72ffaca6cbcb7da7948ea6d024d355c378ceb3ac
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-03-01 12:09:00 +0100

    Implemented unhandled rejections for asynchronous

--- 4_Natours_API/server.js
+++ 4_Natours_API/server.js
@@ -14,13 +14,20 @@ mongoose
     useNewUrlParser: true,
   })
   .then(() => console.log(`DB Conection Successfull`))
-  .catch((err) => console.log(err));
+  .catch((err) => console.log(`Error:`, err));
 // console.log(app.get('env'));
 // console.log(process.env);
 // Server
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}....`);
 });
 
-// TEST
+// Unhandled Rejections in our App
+process.on('unhandledRejection', (err) => {
+  console.log(err.name, err.message);
+  console.log(`UNHANDLED REJECTION, Server is shutting Down`);
+  server.close(() => {
+    process.exit(1);
+  });
+});
