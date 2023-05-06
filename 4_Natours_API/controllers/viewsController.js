d62bed7a0a9d073d099819f8e547fe662dc99878
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-05-06 15:32:30 +0100

    Building the overview -PART1

--- 4_Natours_API/controllers/viewsController.js
+++ 4_Natours_API/controllers/viewsController.js
@@ -1,9 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModels');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  //1 Get tour data from collection
+  const tours = await Tour.find();
+
+  //Build template
+  //3 Render that template using tour data from 1
   res.status(200).render('overview.pug', {
-    tour: 'The forest hikers',
-    user: 'Muyiwa',
+    tour: 'All Tours',
+    tours: tours,
   });
-};
+});
 
 exports.getTour = (req, res) =>
   res.status(200).render('tour.pug', {
