b2b900b1a6a149de27896c402dead210bd62f081
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-05-18 23:26:50 +0100

    Logging in our users with our API PART 3

---
+++ 4_Natours_API/public/js/index.js
@@ -0,0 +1,24 @@
+import '@babel/polyfill';
+import { displayMap } from './mapbox';
+import { login } from './login';
+console.log('Hello from parcel');
+
+/**DOM ELEMENTS */
+const mapBox = document.getElementById('map');
+const loginForm = document.querySelector('.form');
+/**VALUES */
+
+/**DELEGATION */
+if (mapBox) {
+  const locations = JSON.parse(mapBox.dataset.locations);
+  displayMap(locations);
+}
+
+if (loginForm) {
+  loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
--- 4_Natours_API/public/js/login.js
+++ 4_Natours_API/public/js/login.js
@@ -1,9 +1,13 @@
+import axios from 'axios';
+import { showAlert } from './alerts';
+
 const headers = {
   'Cross-Origin-Embedder-Policy': 'require-corp',
   'Cross-Origin-Opener-Policy': 'same-origin',
   'Content-Type': 'application/json',
 };
-const login = async (email, password) => {
+
+export const login = async (email, password) => {
   try {
     const res = await axios({
       method: 'POST',
@@ -15,21 +19,12 @@ const login = async (email, password) => {
       },
     });
     if (res.data.status === 'success') {
-      alert('Logged In successfully');
+      showAlert('successs', 'Logged In successfully');
       window.setTimeout(() => {
         location.assign('/');
       }, 1500);
     }
   } catch (error) {
-    alert(error.response.data.message);
+    showAlert('error', err.response.data.message);
   }
 };
-
-document.querySelector('.form').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  console.log(email);
-  console.log(password);
-  login(email, password);
-});
