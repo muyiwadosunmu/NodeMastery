61e37b9154c663d1dd62afc25cded51e2f71b83c
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-05-18 11:09:29 +0100

    Including a Map with Mapbox 2 but not working for me yet

--- 4_Natours_API/controllers/viewsController.js
+++ 4_Natours_API/controllers/viewsController.js
@@ -15,16 +15,24 @@ exports.getOverview = catchAsync(async (req, res) => {
 
 exports.getTour = catchAsync(async (req, res) => {
   //1 Get the data, for the requested tour(including reviews and guides)
-  const tour = await Tour.findOne({ slug: req.params.slug }).populate({
-    path: 'reviews',
-    fields: 'reviews rating user',
-  });
+  const tour = await Tour.findOne({ slug: req.params.slug })
+    .populate({
+      path: 'reviews',
+      fields: 'reviews rating user',
+    })
+    .exec();
 
   //2 Build the template
 
   //3 Render template using data from step1
-  res.status(200).render('tour.pug', {
-    title: `${tour.name} Tour`,
-    tour: tour,
-  });
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      "default-src 'self' https://*.mapbox.com ;base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src https://cdnjs.cloudflare.com https://api.mapbox.com 'self' blob: ;script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests;"
+    )
+    .render('tour.pug', {
+      title: `${tour.name} Tour`,
+      tour: tour,
+    });
 });
--- 4_Natours_API/public/js/mapbox.js
+++ 4_Natours_API/public/js/mapbox.js
@@ -2,3 +2,48 @@ console.log('Hello from client side');
 
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
 console.log(locations);
+
+mapboxgl.accessToken =
+  'pk.eyJ1IjoibXV5aXdhZGV2IiwiYSI6ImNsaHM1OWFqMTJoYm8zZW9kbjd3aWFybnkifQ.pmM2zU8RlfKbFGaq2Y--Ng';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/muyiwadev/clhs5s2w0003g01pn97ur65ci',
+  scrollZoom: false,
+  /* center:[-118.113491, 34.111745],
+    zoom:4,
+    interactive:false */
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+locations.forEach((loc) => {
+  //Create marker
+  const el = document.createElement('div');
+  el.className = 'marker';
+  //Add Marker
+  new mapboxgl.Marker({
+    element: el,
+    anchor: 'bottom',
+  })
+    .setLngLat(loc.coordinates)
+    .addTo(map);
+
+  // Add popup
+  new mapboxgl.Popup({
+    offset: 30,
+  })
+    .setLngLat(loc.coordinates)
+    .setHTML(`<p>Date  ${loc.day}: ${loc.description}</p>`)
+    .addTo(map);
+
+  //Extend map bounds to include current loction
+  bounds.extend(loc.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: '200',
+    bottom: '150',
+    right: '100',
+    left: '200',
+  },
+});
