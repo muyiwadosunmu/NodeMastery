23915abf6a9ae00431fe4274cc1cf7610f2c3b8c
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-05-18 11:23:17 +0100

    Building the Login Screen

--- 4_Natours_API/Routes/viewRoutes.js
+++ 4_Natours_API/Routes/viewRoutes.js
@@ -6,4 +6,5 @@ const router = express.Router();
 
 router.get('/', viewsController.getOverview);
 router.get('/tour/:slug', viewsController.getTour);
+router.get('/login', viewsController.getLoginForm);
 module.exports = router;
