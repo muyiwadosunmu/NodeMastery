34203bb3be866c1238655565bfb02f8923e567a2
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2023-05-19 16:21:34 +0100

    Password Update and end of server side rendering

--- 4_Natours_API/public/js/index.js
+++ 4_Natours_API/public/js/index.js
@@ -1,6 +1,6 @@
 import '@babel/polyfill';
 import { displayMap } from './mapbox';
-import { updateData } from './updateSettings';
+import { updateSettings } from './updateSettings';
 import { login, logout } from './login';
 console.log('Hello from parcel');
 
@@ -9,6 +9,7 @@ const mapBox = document.getElementById('map');
 const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 const userDataForm = document.querySelector('.form-user-data');
+const userPasswordForm = document.querySelector('.form-user-data');
 /**VALUES */
 
 /**DELEGATION */
@@ -33,5 +34,23 @@ if (userDataForm)
     e.preventDefault();
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    updateData(name, email);
+    updateSettings({ name, email }, 'data');
+  });
+
+if (userPasswordForm)
+  userPasswordForm.addEventListener('submit', async (e) => {
+    document.querySelector('.btn--save-password').textContent = 'Updating...';
+    e.preventDefault();
+    const passwordCurrent = document.getElementById('password-current').value;
+    const password = document.getElementById('password').value;
+    const passwordConfirm = document.getElementById('password-confirm').value;
+    await updateSettings(
+      { passwordCurrent, password, passwordConfirm },
+      'password'
+    );
+
+    document.querySelector('.btn--save-password').textContent = 'Save password';
+    document.getElementById('password-current').value = '';
+    document.getElementById('password').value = '';
+    document.getElementById('password-confirm').value = '';
   });
